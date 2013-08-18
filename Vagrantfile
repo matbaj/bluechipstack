19ec13a5217c22b4273bbb1bbfa729932266bbc7
Kord Campbell <kordless@gmail.com>
2013-08-17 17:45:31 -1000

    setup script done

---
+++ Vagrantfile
@@ -0,0 +1,61 @@
+# -*- mode: ruby -*-
+# vi: set ft=ruby :
+
+BOX_NAME = ENV['BOX_NAME'] || "ubuntu"
+BOX_URI = ENV['BOX_URI'] || "http://files.vagrantup.com/precise64.box"
+VF_BOX_URI = ENV['BOX_URI'] || "http://files.vagrantup.com/precise64_vmware_fusion.box"
+FORWARD_PORTS = ENV['FORWARD_PORTS']
+
+Vagrant::Config.run do |config|
+  # Setup virtual machine box in bridged mode. 
+  # config.vm.network :bridged
+  config.vm.network :hostonly, "10.0.10.150"
+  config.vm.box = BOX_NAME
+  config.vm.box_url = BOX_URI
+  #config.vm.forward_port 8443,443 
+
+  # Provision and install new kernel if deployment was not done
+  if Dir.glob("#{File.dirname(__FILE__)}/.vagrant/machines/default/*/id").empty?
+    pkg_cmd = "apt-get -y update;"
+    pkg_cmd << "apt-get -y install git;"
+    pkg_cmd << "apt-get -y install curl;"
+    pkg_cmd << "apt-get -y install vim;"
+    pkg_cmd << "git clone https://github.com/bluechiptek/bluechipstack.git;"
+    pkg_cmd << "cp /vagrant/setuprc ~/bluechipstack/;"
+    pkg_cmd << "echo 'source ~/bluechipstack/setuprc' >> ~/.bash_profile;"
+    pkg_cmd << "curl -s -L https://raw.github.com/rcbops/support-tools/master/chef-install/install-chef-server.sh | bash ;"
+    pkg_cmd << "curl -s -L https://raw.github.com/rcbops/support-tools/master/chef-install/install-cookbooks.sh | bash;"
+    config.vm.provision :shell, :inline => pkg_cmd
+  end
+end
+
+# Providers were added on Vagrant >= 1.1.0
+Vagrant::VERSION >= "1.1.0" and Vagrant.configure("2") do |config|
+  config.vm.provider :rackspace do |rs|
+    config.ssh.private_key_path = ENV["RS_PRIVATE_KEY"]
+    rs.username = ENV["RS_USERNAME"]
+    rs.api_key  = ENV["RS_API_KEY"]
+    rs.public_key_path = ENV["RS_PUBLIC_KEY"]
+    rs.flavor   = /512MB/
+    rs.image    = /Ubuntu/
+  end
+
+  config.vm.provider :virtualbox do |vb|
+    config.vm.box = BOX_NAME
+    config.vm.box_url = BOX_URI
+  end
+end
+
+if !FORWARD_PORTS.nil?
+  Vagrant::VERSION < "1.1.0" and Vagrant::Config.run do |config|
+    (49000..49900).each do |port|
+      config.vm.forward_port port, port
+    end
+  end
+
+  Vagrant::VERSION >= "1.1.0" and Vagrant.configure("2") do |config|
+    (49000..49900).each do |port|
+      config.vm.network :forwarded_port, :host => port, :guest => port
+    end
+  end
+end
--- bluechip_openstack_install.sh
+++ bluechip_openstack_install.sh
@@ -19,8 +19,7 @@ echo;
 read -p "How many total nodes in this install? " -r
 if [ $REPLY -eq $REPLY 2>/dev/null ]
 then
-  num_nodes=$REPLY
-  echo "export NUMBER_NODES="$REPLY > setuprc
+  BCT_NUMBER_NODES=$REPLY
 else
   echo;
   echo "You need to enter an integer value."
@@ -28,35 +27,38 @@ else
 fi
 
 # prompt for a few things we'll need for setup
-for (( x=1; x<=$num_nodes; x++ ))
+for (( x=1; x<=$BCT_NUMBER_NODES; x++ ))
   do
     read -p "Enter a short hostname for node #"$x": "
-    echo "export NODE_"$x"_HOSTNAME="$REPLY >> setuprc
+    echo "NODE_"$x"_HOSTNAME="$REPLY >> setuprc
     read -p "Enter the IP address for node #"$x" ($REPLY): "
-    echo "export NODE_"$x"_IP="$REPLY >> setuprc
+    echo "NODE_"$x"_IP="$REPLY >> setuprc
   done
+read -p "Enter the email address for service accounts (nova, glance, keystone, quantum, etc.): " email
+echo;
+read -p "Enter a short name to use for your default region: " region
+echo;
 
 # making a unique token for this install
-if [ -a "/sbin/md5" ]
-then
-  token=`cat /dev/urandom | head -c2048 | md5 | cut -d' ' -f1`
-else
-  token=`cat /dev/urandom | head -c2048 | md5sum | cut -d' ' -f1`
-fi
-echo "export ROOT_PASSWD="$token >> setuprc
+token=`cat /dev/urandom | head -c2048 | md5sum | cut -d' ' -f1`
 
-echo;
-echo "#############################################################################################################"
-echo;
+cat > setuprc <<EOF
+export SG_INSTALL_SWIFT=$SG_INSTALL_SWIFT
+export SG_MULTI_NODE=$SG_MULTI_NODE
+export SG_SERVICE_EMAIL=$email
+export SG_SERVICE_PASSWORD=$password
+export SG_SERVICE_TOKEN=$token
+export SG_SERVICE_REGION=$region
+EOF
+
 echo "Using the following for setup.  Edit 'setuprc' if you don't like what you see."
-echo;
-echo "#############################################################################################################"
-echo;
+echo
 cat setuprc
-echo;
+echo
+
 echo "#############################################################################################################"
 echo;
-echo "Setup configuration complete.  Continue the setup by doing a 'vagrant up'."
+echo "Setup configuration complete.  Continue the setup by doing a './bluechip_openstack_install.sh'."
 echo;
 echo "#############################################################################################################"
 echo;
